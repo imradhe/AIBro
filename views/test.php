d0a92881ff677780898b17c4064c08b4f3f9747a
imradhe <radhesyam.musicals@gmail.com>
2023-06-03 02:16:26 +0530

    Tested AIBro

--- views/test.php
+++ views/test.php
@@ -1,21 +1,15 @@
-<?php
-controller('AIBro');
-
-$userMessage = "Best colleges for engineering in chennai";
-
-
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>ChatGPT Typing Effect</title>
+  <title>AI Bro - AI Chat Assitant for Career Guidance & College Counseling</title>
   <style>
     .chat-container {
       border: 1px solid #ccc;
       padding: 10px;
       white-space: pre-wrap !important;
       max-width: 20vw;
+      max-height: 300px;
+      overflow-y: scroll;
     }
     .message {
       margin-bottom: 10px;
@@ -24,19 +18,14 @@ $userMessage = "Best colleges for engineering in chennai";
 </head>
 <body>
   <div class="chat-container">
-    <div class="message">
-        <div class="user"></div>
-        <div class="AIBro">AI Bro: <span id="typing"></span></div>
-    </div>
+    <div class="message">User: <textarea type="text" id="user-input"></textarea></div>
+    <div class="message">AI Bro: <span id="typing"></span></div>
   </div>
+  <button id="send-button">Send Message</button>
 
   <script>
-    // Simulate typing effect for HTML content
     function typeHTML(html, speed) {
-      userMessage = "<?php echo $userMessage; ?>"
-      document.querySelector('.user').innerHTML = "User:" + userMessage 
-
-      const typingElement = document.querySelector('#typing');
+      const typingElement = document.getElementById('typing');
       typingElement.innerHTML = ''; // Clear previous content
       let index = 0;
       const typingInterval = setInterval(() => {
@@ -48,13 +37,40 @@ $userMessage = "Best colleges for engineering in chennai";
       }, speed);
     }
 
-    // Delayed response from AI Bro
-    setTimeout(() => {
-      aiResponse = "<?php echo AIBro::response($userMessage); ?>";
-      aiResponse = aiResponse.replace(/<br\s*[\/]?>/gi, "\n");
-      const typingSpeed = 10; // Adjust typing speed (milliseconds per character)
-      typeHTML(aiResponse, typingSpeed);
-    }, 1500); // Adjust delay time (milliseconds)
+    function sendRequest() {
+      const userInput = document.getElementById('user-input').value;
+      if (userInput.trim() === '') return;
+      
+      const xhr = new XMLHttpRequest();
+      const url = '<?php echo route('api/aibro')?>';
+      const data = new FormData();
+      data.append('message', userInput);
+
+      xhr.open('POST', url, true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            if (response.status === '200') {
+              aiResponse = response.data;
+              aiResponse = aiResponse.replace(/<br\s*[\/]?>/gi, "\n");
+              const typingSpeed = 10; // Adjust typing speed (milliseconds per character)
+              typeHTML(aiResponse, typingSpeed);
+            }
+          } else {
+            console.log('Error: ' + xhr.status);
+          }
+        }
+      };
+      xhr.send(data);
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+      const sendButton = document.getElementById('send-button');
+      sendButton.addEventListener('click', function() {
+        sendRequest();
+      });
+    });
   </script>
 </body>
 </html>
