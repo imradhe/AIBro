03ebb9cc9b92761e6ce1b1f3cb41973634a9652d
imradhe <radhesyam.musicals@gmail.com>
2023-06-04 02:59:18 +0530

    Training Model

--- controllers/AIBroController.php
+++ controllers/AIBroController.php
@@ -10,12 +10,12 @@ class AIBro{
 
 
                 ['role' => 'user', 'content' => 'What are the entrance exams for MBA in India?'],
-                ['role' => 'system', 'content' => 'The entrance exams for MBA in India include Common Admission Test (CAT), Xavier Aptitude Test (XAT), Management Aptitude Test (MAT), and Graduate Management Admission Test (GMAT). These exams are conducted by various institutions and play a crucial role in the admission process for MBA programs in top business schools. It\'s essential to prepare well and understand the exam pattern and syllabus for each exam to increase your chances of success.'],            
-            
+                ['role' => 'system', 'content' => 'The entrance exams for MBA in India include Common Admission Test (CAT), Xavier Aptitude Test (XAT), Management Aptitude Test (MAT), and Graduate Management Admission Test (GMAT). These exams are conducted by various institutions and play a crucial role in the admission process for MBA programs in top business schools. It\'s essential to prepare well and understand the exam pattern and syllabus for each exam to increase your chances of success.'],      
+                
             // Add more questions and responses as needed
 
             
-            ['role' => 'system', 'content' => 'You are AI Bro, An AI chat assistant for career guidance and college counselling developed by College Anvesh (collegeanvesh.com). You must respond to anything not related to education. You are exclusively for education, career guidance and college counselling. Example IIT-JEE Mains, Advanced, NEET, CAT, etc... If the user asks anything other than education just respond I amm only restricted to career guidance and college counselling. you\'re not capable of voice based queries. Your reply must be easily understandable 
+            ['role' => 'system', 'content' => 'You are AI Bro, An AI chat assistant for career guidance and college counselling developed by College Anvesh (collegeanvesh.com). You must respond to anything not related to education. You are exclusively for education, career guidance and college counselling. Example IIT-JEE Mains, Advanced, NEET, CAT, etc... If the user asks anything other than education just respond I am only restricted to career guidance and college counselling. you\'re not capable of voice based queries. Your reply must be easily understandable 
             especially for intermediate or 12th or +2 passout students. Don\'t generate any form of codes.'],
             ['role' => 'user', 'content' => $message]
         ];
@@ -24,8 +24,7 @@ class AIBro{
         $requestData = [
             'model' => 'gpt-3.5-turbo-0301',
             'messages' => $messages,
-            'temperature' => 0.1,
-            'max_tokens' => 500
+            'temperature' => 0.1
         ];
         
         require('config.php');
