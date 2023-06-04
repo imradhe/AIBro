213d00d62db0a3586407a0da0de73a7e8bd7f79f
imradhe <radhesyam.musicals@gmail.com>
2023-06-05 00:05:52 +0530

    Fixed API

--- views/api/aibro.php
+++ views/api/aibro.php
@@ -2,16 +2,16 @@
 controller('AIBro');
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    $data = AIBro::response($_POST['message']);
     
-    if (!empty($data)) {
+    if($_POST['count'] >= 5 || $_POST['tokens'] >= 2000){
+        $data = "Daily Limit of 5 conversations or 2000 tokens exceeded. Try again after 24 hrs\n";
         $res['status'] = '200';
-        $res['message'] = 'Success';
+        $res['message'] = 'Limit Exceeded';
         $res['data'] = $data;
     } else {
-        $res['status'] = '500';
-        $res['message'] = "No Data";
-        $res['data'] = $data;
+        $res['status'] = '200';
+        $res['message'] = "Success";
+        $res['data'] = AIBro::response($_POST['message']);
     }
 
 http_response_code($res['status']);
@@ -20,5 +20,3 @@ echo json_encode($res);
 
 }
 else unauthorized();
-
-// I want to know bes institutes for GATE coaching in Chennai
