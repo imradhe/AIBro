0e8481130662b3a4a83b85203b80f34c9ac06486
imradhe <radhesyam.musicals@gmail.com>
2023-06-06 03:53:10 +0530

    Redesigned AI Bro

--- views/test.php
+++ views/test.php
@@ -1,175 +1,190 @@
+<?php 
+
+?>
 <!DOCTYPE html>
-<html>
-<head>
-  <title>AI Bro - AI Chat Assistant for Career Guidance & College Counseling</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<html lang="en">
   <style>
-    .chat-container {
-      border: 1px solid #ccc;
-      padding: 10px;
-      white-space: pre-wrap !important;
+    body {
+      overflow-x: hidden;
     }
-    .message {
-      margin-bottom: 10px;
+    .clear {
+      position: fixed;
+      bottom: 20px;
+      left: calc(
+        12% - 60px
+      ); /* Position the clear button 60px to the left of the prompt field */
+      z-index: 1; /* Ensure the clear button appears above the prompt field */
     }
-    textarea {
-      min-width: 100%;
+
+    .prompt {
+      position: fixed;
+      bottom: 20px;
+      max-width: 80vw;
+      margin-left: 12%;
+      height: 50px;
+      min-height: 50px;
+      max-height: 100px;
+      resize: none;
     }
-    .history-container {
-      white-space: pre-wrap !important;
-      margin-bottom: 20px;
+    .send {
+      position: fixed;
+      bottom: 25px;
+      right: calc(8% + 10px);
+      transform: rotate(45deg);
+    }
+
+    .examples button {
+      font-size: 14px;
+    }
+
+    .conversations {
+      margin-bottom: 200px;
+    }
+
+    .conversation .role {
+      font-size: 14px;
+    }
+    .conversation .message {
+      border-radius: 10px;
+      padding: 2%;
+      white-space: pre-wrap;
+    }
+
+    .bg-ai {
+      background-color: #5454541f;
+    }
+    .bg-you {
+      background-color: #a6a6a68f;
+      color: #000;
+    }
+    @media screen and (max-width: 440px) {
+      /* Adjust the positioning for smaller screens */
+      .clear {
+        left: 10px;
+      }
+
+      .prompt {
+        max-width: 80vw;
+        margin-left: 16%;
+      }
+
+      .send {
+        right: calc(5% + 10px);
+      }
+    }
+
+    @media screen and (max-width: 380px) {
+      /* Adjust the positioning for extra smaller screens */
+      .prompt {
+        max-width: 72vw;
+        margin-left: 24%;
+      }
+
+      .send {
+        right: calc(4% + 10px);
+      }
     }
   </style>
-</head>
-<body>
-  <div class="container">
-    <div class="history-container">
-      <div id="history-list"></div>
-      <button id="reset-button" class="btn btn-danger">Clear Conversations</button>
+  <?php include("views/partials/head.php"); ?>
+
+  <body>
+    <?php include("views/partials/nav.php"); ?>
+
+    <div class="container">
+      <div class="mt-5 logo text-center">
+        <img
+          src="https://placehold.co/120"
+          alt="AI Bro Logo"
+          class="img-fluid my-2 mt-5"
+        />
+        <h4>
+          Your AI Assistant for Career Guidance <br />
+          & College Counseling
+        </h4>
+      </div>
+
+      <div class="examples text-center mt-5">
+        <h5 class="text-secondary pt-5">Try asking about</h5>
+
+        <div class="d-flex flex-column align-items-center">
+          <button class="btn btn-outline-secondary rounded-pill my-2">
+            "Top 10 IITs for Computer Science" →
+          </button>
+          <button class="btn btn-outline-secondary rounded-pill my-2">
+            "Preparation strategy for JEE Mains" →
+          </button>
+          <button class="btn btn-outline-secondary rounded-pill my-2">
+            "Rank Prediction for JEE Mains" →
+          </button>
+        </div>
+      </div>
+
+      <div class="conversations mt-5 pb-5">
+        <div class="conversation mb-5">
+          <div class="ai text-secondary">AI Bro</div>
+          <div class="message bg-ai"></div>
+        </div>
+      </div>
     </div>
-    <div class="chat-container">
-      <div class="message">AI Bro: <span id="typing"></span></div>
-      <div class="info"></div>
-    </div>
-  
-    <textarea type="text" id="user-input" class="form-control"></textarea>
-  
-    <button id="send-button" class="btn btn-primary">Send Message</button>
-  </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-  <script>
-    function typeHTML(html, speed) {
-      const typingElement = document.getElementById('typing');
-      typingElement.innerHTML = ''; // Clear previous content
-      let index = 0;
-      const typingInterval = setInterval(() => {
-        typingElement.innerHTML += html[index];
-        index++;
-        if (index >= html.length) {
-          clearInterval(typingInterval);
-          enableButton();
-        }
-      }, speed);
-    }
+    <button class="btn btn-primary rounded-circle clear ms-2" id="clear">
+      <i class="bi bi-eraser-fill fs-5"></i>
+    </button>
 
-    function saveToLocalStorage(message, id, total_tokens, finish_reason, time, aiResponse) {
-      const conversation = {
-        message: message,
-        id: id,
-        total_tokens: total_tokens,
-        finish_reason: finish_reason,
-        time: time,
-        aiResponse: aiResponse
-      };
-      let conversations = localStorage.getItem('conversations');
-      if (conversations) {
-        conversations = JSON.parse(conversations);
-      } else {
-        conversations = [];
+    <textarea
+      name="prompt"
+      id="prompt"
+      class="form-control prompt rounded-pill ps-4 pt-3"
+      placeholder="Send a message"
+    ></textarea>
+    <button id="send" class="btn btn-transparent send">
+      <i class="bi bi-send-fill"></i>
+    </button>
+
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="<?php assets('js/app.js');?>"></script>
+    <script>
+
+      async function sendRequest(promptValue){
+        let url = '<?php echo route('api/aibro') ?>'
+        let data = new FormData()
+        data.append('message', promptValue)
+        data.append('count', count())
+        data.append('tokens', tokenCount())
+
+
+        try {
+                  let response = await axios.post(url, data)
+
+                  if (response.status === 200) {
+                      let responseData = response.data
+
+                      if (responseData.status === '200' && responseData.message === 'Success') {
+                          let aiResponse = responseData.data.response.replace(/<br\s*[\/]?>/gi, '\n')
+                          let id = responseData.data[0].id
+                          let total_tokens = responseData.data[0].usage.total_tokens
+                          let finish_reason = responseData.data[0].choices[0].finish_reason
+                          let time = responseData.data[0].created
+
+                          return {
+                          promptValue: promptValue,
+                          id: id,
+                          total_tokens: total_tokens,
+                          finish_reason: finish_reason,
+                          time: time,
+                          aiResponse: aiResponse
+                        }
+
+                      } else {
+                        return false
+                      }
+                  }
+              } catch (error) {
+                  console.log('Error: ' + error)
+                  return false
+              }
       }
-      conversations.push(conversation);
-      localStorage.setItem('conversations', JSON.stringify(conversations));
-    }
-
-    function renderChatHistory() {
-      const historyList = document.getElementById('history-list');
-      historyList.innerHTML = '';
-
-      const conversations = JSON.parse(localStorage.getItem('conversations'));
-      if (!conversations) return;
-
-      conversations.forEach(function(conversation) {
-        const card = document.createElement('div');
-        card.className = 'card mb-2';
-        const cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-        const userPrompt = document.createElement('div');
-        userPrompt.className = 'card-text';
-        userPrompt.innerHTML = '<b>User:</b> ' + conversation.message;
-        const aiResponse = document.createElement('div');
-        aiResponse.className = 'card-text';
-        aiResponse.innerHTML = '<b>AI Bro:</b> ' + conversation.aiResponse;
-
-        cardBody.appendChild(userPrompt);
-        cardBody.appendChild(aiResponse);
-        card.appendChild(cardBody);
-        historyList.appendChild(card);
-      });
-    }
-
-    function resetConversations() {
-      localStorage.removeItem('conversations');
-      renderChatHistory();
-    }
-
-    async function sendRequest() {
-      disableButton();
-      let userInput = document.getElementById('user-input').value;
-      if (userInput.trim() === '') return;
-
-      const url = '<?php echo route('api/aibro')?>';
-      const data = new FormData();
-      data.append('message', userInput);
-
-      try {
-        const response = await axios.post(url, data);
-        if (response.status === 200) {
-          const responseData = response.data;
-          if (responseData.status === '200') {
-            let aiResponse = responseData.data.response;
-            aiResponse = aiResponse.replace(/<br\s*[\/]?>/gi, "\n");
-            const typingSpeed = 10; // Adjust typing speed (milliseconds per character)
-            const id = responseData.data[0].id;
-            const total_tokens = responseData.data[0].usage.total_tokens;
-            const finish_reason = responseData.data[0].choices[0].finish_reason;
-            const time = responseData.data[0].created;
-
-            typeHTML(aiResponse, typingSpeed);
-
-            saveToLocalStorage(userInput, id, total_tokens, finish_reason, time, aiResponse);
-            renderChatHistory();
-
-            userInput = document.querySelector('#user-input');
-            userInput.value = '';
-            userInput.focus();
-          }
-        } else {
-          console.log('Error: ' + response.status);
-        }
-      } catch (error) {
-        console.log('Error: ' + error);
-      }
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-      const sendButton = document.getElementById('send-button');
-      sendButton.addEventListener('click', function() {
-        sendRequest();
-      });
-
-      const resetButton = document.getElementById('reset-button');
-      resetButton.addEventListener('click', function() {
-        resetConversations();
-      });
-
-      renderChatHistory();
-      document.getElementById('user-input').focus();
-    });
-
-    function disableButton() {
-      const sendButton = document.getElementById('send-button');
-      sendButton.disabled = true;
-      sendButton.textContent = 'Loading...';
-    }
-
-    function enableButton() {
-      const sendButton = document.getElementById('send-button');
-      sendButton.disabled = false;
-      sendButton.textContent = 'Send Message';
-      document.getElementById('user-input').focus();
-      document.getElementById('user-input').scrollIntoView({ behavior: 'smooth' });
-    }
-  </script>
-</body>
+    
+      </script>
+  </body>
 </html>
