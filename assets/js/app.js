7ce7ed1bbabe4af4ee87db1b964eac07c9deada5
imradhe <radhesyam.musicals@gmail.com>
2023-06-06 03:53:27 +0530

    Redesigned AI Bro

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,63 @@
+const prompt = document.querySelector("#prompt");
+const clear = document.querySelector("#clear");
+const send = document.querySelector("#send");
+const conversations = document.querySelector(".conversations");
+const allConversations = document.querySelector(".conversation");
+let lastConversation = document.querySelector(
+  ".conversations .conversation:last-child .message"
+);
+
+prompt.addEventListener("keyup", function () {
+  prompt.style.height = "auto";
+  prompt.style.height = prompt.scrollHeight + "px";
+  prompt.classList.remove("rounded-pill");
+
+  if (prompt.value === "") {
+    prompt.style.height = "";
+    prompt.classList.add("rounded-pill");
+  }
+
+  send.classList.toggle("text-success", prompt.value !== "");
+});
+
+prompt.addEventListener("focus", function () {
+  prompt.style.height = "auto";
+  prompt.style.height = prompt.scrollHeight + "px";
+  prompt.classList.remove("rounded-pill");
+
+  if (prompt.value === "") {
+    prompt.style.height = "";
+    prompt.classList.add("rounded-pill");
+  }
+
+  send.classList.toggle("text-success", prompt.value !== "");
+});
+
 function typeHTML(html, speed) {
-  let typingElement = document.getElementById("typing");
-  typingElement.innerHTML = ""; // Clear previous content
+  lastConversation = document.querySelector(
+    ".conversations .conversation:last-child .message"
+  );
+  lastConversation.innerHTML = ""; // Clear previous content
   let index = 0;
   let typingInterval = setInterval(() => {
-    typingElement.innerHTML += html[index];
+    lastConversation.innerHTML += html[index];
     index++;
     if (index >= html.length) {
       clearInterval(typingInterval);
-      enableButton();
+      enable();
     }
   }, speed);
 }
 
-function saveToLocalStorage(
-  message,
-  id,
-  total_tokens,
-  finish_reason,
-  time,
-  aiResponse
-) {
+function greetings() {
+  let greeting =
+    "Hello! I am AI Bro, an AI chat assistant for career guidance and college counseling. \n\nHow may I help you today?";
+  typeHTML(greeting, 10);
+}
+
+function save(prompt, id, total_tokens, finish_reason, time, aiResponse) {
   let conversation = {
-    message: message,
+    prompt: prompt,
     id: id,
     total_tokens: total_tokens,
     finish_reason: finish_reason,
@@ -29,144 +65,183 @@ function saveToLocalStorage(
     aiResponse: aiResponse,
     removed: false, // Set removed attribute to false initially
   };
-  let conversations = localStorage.getItem("conversations");
-  if (conversations) {
-    conversations = JSON.parse(conversations);
-  } else {
-    conversations = [];
-  }
-  conversations.push(conversation);
-  localStorage.setItem("conversations", JSON.stringify(conversations));
+
+  let getConversations =
+    JSON.parse(localStorage.getItem("conversations")) || [];
+  getConversations.push(conversation);
+  console.log(getConversations);
+  set = localStorage.setItem("conversations", JSON.stringify(getConversations));
+
+  return set;
 }
 
-function renderChatHistory() {
-  let historyList = document.getElementById("history-list");
-  historyList.innerHTML = "";
+function render() {
+  let getConversations =
+    JSON.parse(localStorage.getItem("conversations")) || [];
+  let html = "";
 
-  let conversations = JSON.parse(localStorage.getItem("conversations"));
-  if (!conversations) return;
-
-  conversations.forEach(function (conversation) {
+  for (const conversation of getConversations) {
     if (!conversation.removed) {
-      // Display conversations with removed attribute set to false
-      let card = document.createElement("div");
-      card.className = "card mb-2";
-      let cardBody = document.createElement("div");
-      cardBody.className = "card-body";
-      let userPrompt = document.createElement("div");
-      userPrompt.className = "card-text";
-      userPrompt.innerHTML = "<b>User:</b> " + conversation.message;
-      let aiResponse = document.createElement("div");
-      aiResponse.className = "card-text";
-      aiResponse.innerHTML = "<b>AI Bro:</b> " + conversation.aiResponse;
+      html += `
+        <div class="conversation mb-3">
+          <div class="you text-secondary">You</div>
+          <div class="message bg-you">${conversation.prompt}</div>
+        </div>
 
-      cardBody.appendChild(userPrompt);
-      cardBody.appendChild(aiResponse);
-      card.appendChild(cardBody);
-      historyList.appendChild(card);
+        <div class="conversation mb-5">
+          <div class="ai text-secondary">AI Bro</div>
+          <div class="message bg-ai">${conversation.aiResponse}</div>
+        </div>
+      `;
     }
-  });
-}
-
-function resetConversations() {
-  let conversations = JSON.parse(localStorage.getItem("conversations"));
-  if (conversations) {
-    conversations.forEach(function (conversation) {
-      conversation.removed = true; // Set removed attribute to true for all conversations
-    });
-    localStorage.setItem("conversations", JSON.stringify(conversations));
   }
-  document.querySelector("#typing").innerHTML = "";
-  renderChatHistory();
 
-  sendGreetingMessage();
-  document.querySelector("#user-input").focus();
+  conversations.innerHTML = html;
+
+  if (conversations.innerHTML === "") {
+    conversations.innerHTML += `
+      <div class="conversation mb-5">
+        <div class="ai text-secondary">AI Bro</div>
+        <div class="message bg-ai"></div>
+      </div>
+      `;
+    if (greetings()) enable();
+  }
+  
+
+  lastConversation = document.querySelector(
+    ".conversations .conversation:last-child .message"
+  );
+  lastConversation.scrollIntoView({ behavior: "smooth" });
 }
 
-function disableButton() {
-  let sendButton = document.getElementById("send-button");
-  let resetButton = document.getElementById("reset-button");
-  sendButton.disabled = true;
-  resetButton.disabled = true;
-  sendButton.innerHTML =
+function reset() {
+  conversations.innerHTML = "";
+  disable();
+  localStorage.removeItem("conversations");
+  conversations.innerHTML += `
+  <div class="conversation mb-5">
+    <div class="ai text-secondary">AI Bro</div>
+    <div class="message bg-ai"></div>
+  </div>
+  `;
+  if (greetings()) enable();
+}
+
+function clearConversations() {
+  conversations.innerHTML = "";
+  disable();
+  let getConversations = JSON.parse(localStorage.getItem("conversations"));
+  //console.log(getConversations)
+  getConversations.forEach(conversation=> {
+    conversation.removed = true
+    console.log(conversation)
+  })
+  localStorage.setItem("conversations", JSON.stringify(getConversations));
+  conversations.innerHTML += `
+    <div class="conversation mb-5">
+      <div class="ai text-secondary">AI Bro</div>
+      <div class="message bg-ai"></div>
+    </div>
+    `;
+  if (greetings()) enable();
+}
+
+clear.addEventListener("click", clearConversations);
+
+send.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const promptValue = prompt.value;
+  conversations.innerHTML += `
+    <div class="conversation mb-3">
+      <div class="you text-secondary">You</div>
+      <div class="message bg-you">${promptValue}</div>
+    </div>
+  `;
+  prompt.value = "";
+
+  disable();
+
+  const response = await sendRequest(promptValue);
+
+  if (response) {
+    conversations.innerHTML += `
+    <div class="conversation mb-3">
+      <div class="ai text-secondary">AI Bro</div>
+      <div class="message bg-ai"></div>
+    </div>
+  `;
+
+    lastConversation.scrollIntoView({ behavior: "smooth" });
+    save(
+      response.promptValue,
+      response.id,
+      response.total_tokens,
+      response.finish_reason,
+      response.time,
+      response.aiResponse
+    );
+    if (typeHTML(response.aiResponse, 10)) enable();
+  } else {
+    conversations.innerHTML += `
+    <div class="conversation mb-3">
+      <div class="ai text-secondary">AI Bro</div>
+      <div class="message bg-ai"></div>
+    </div>
+  `;
+    errorMsg =
+      "Daily Limit of 5 conversations or 2000 tokens exceeded. Try again after 24 hrs";
+    if (typeHTML(errorMsg, 10)) enable();
+  }
+});
+
+function disable() {
+  send.disabled = true;
+  clear.disabled = true;
+  send.innerHTML =
     '<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>';
 }
 
-function enableButton() {
-  let sendButton = document.getElementById("send-button");
-  let resetButton = document.getElementById("reset-button");
-  sendButton.disabled = false;
-  resetButton.disabled = false;
-  sendButton.innerHTML = '<i class="bi bi-send-fill"></i>';
-  document.getElementById("user-input").focus();
-  document.getElementById("user-input").scrollIntoView({ behavior: "smooth" });
-}
-
-function sendGreetingMessage() {
-  let greeting =
-    "Hello! I am AI Bro, an AI chat assistant for career guidance and college counseling. \n\nHow may I help you today?";
-  typeHTML(greeting, 10);
-
-  let time = new Date().toISOString();
+function enable() {
+  send.disabled = false;
+  clear.disabled = false;
+  send.innerHTML = '<i class="bi bi-send-fill"></i>';
+  prompt.focus();
+  lastConversation.scrollIntoView({ behavior: "smooth" });
 }
 
 function count() {
-  let conversations = JSON.parse(localStorage.getItem("conversations"));
+  let conversations = JSON.parse(localStorage.getItem("conversations")) || [];
   let currentTime = new Date().getTime();
   let twentyFourHoursAgo = currentTime - 24 * 60 * 60 * 1000; // 24 hours ago in milliseconds
   let count = 0;
 
-  if (conversations) {
-    conversations.forEach(function (conversation) {
-      let conversationTime = conversation.time;
-      if (twentyFourHoursAgo >= conversationTime) {
-        count++;
-      }
-    });
+  for (const conversation of conversations) {
+    let conversationTime = conversation.time;
+    if (twentyFourHoursAgo >= conversationTime) {
+      count++;
+    }
   }
 
   return count;
 }
 
 function tokenCount() {
-  let conversations = JSON.parse(localStorage.getItem("conversations"));
+  let conversations = JSON.parse(localStorage.getItem("conversations")) || [];
   let currentTime = new Date().getTime();
   let twentyFourHoursAgo = currentTime - 24 * 60 * 60 * 1000; // 24 hours ago in milliseconds
   let count = 0;
 
-  if (conversations) {
-    conversations.forEach(function (conversation) {
-      let tokens = conversation.total_tokens;
-      if (twentyFourHoursAgo >= tokens) {
-        count += tokens;
-      }
-    });
+  for (const conversation of conversations) {
+    let tokens = conversation.total_tokens;
+    if (twentyFourHoursAgo >= tokens) {
+      count += tokens;
+    }
   }
 
   return count;
 }
 
-function limitReached() {
-  errorMsg =
-    "Daily Limit of 5 conversations or 2000 tokens exceeded. Try again after 24 hrs";
-  typeHTML(errorMsg, 10);
-  return enableButton();
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  let sendButton = document.getElementById("send-button");
-  sendButton.addEventListener("click", function () {
-    sendRequest();
-  });
-
-  let resetButton = document.getElementById("reset-button");
-  resetButton.addEventListener("click", function () {
-    resetConversations();
-  });
-
-  renderChatHistory();
-  document.getElementById("user-input").focus();
-
-  sendGreetingMessage();
-  document.getElementById("user-input").focus();
+window.addEventListener("load", function () {
+  render();
 });
