1236db0872c4c3824b39461d7d13ff633672650b
imradhe <radhesyam.musicals@gmail.com>
2023-06-16 18:24:04 +0530

    Changed Icon

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-sm">
     <div class="container">
-        <b><a class="navbar-brand" href="<?php echo home(); ?>"><?php echo $config['APP_NAME'] ?></a></b>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        <b><a class="navbar-brand" href="<?php echo home(); ?>"> <img src="<?php assets('img/AIBroIcon.png');?>" alt="AI Bro Icon" class="img-fluid" style="max-height: 40px"> <?php echo $config['APP_NAME'] ?></a></b>
+        <button class="navbar-toggler border-0 rounded-circle p-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <i class="bi bi-list"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <?php if (url() != route('login')) { ?>
