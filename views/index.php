4b49c6ebbd29e14c374717260151ee7c656e82a7
imradhe <radhesyam.musicals@gmail.com>
2023-06-16 18:18:08 +0530

    Small Changes

--- views/index.php
+++ views/index.php
@@ -11,29 +11,29 @@
 
     <div class="container">
       <div class="mt-5 logo text-center">
-        <img
+        <a href="<?php echo home()?>"><span class="logo" style="font-size:160px"><img
           src="<?php assets('img/AIBro.png');?>"
           alt="AI Bro Logo"
           class="img-fluid my-2 mt-5"
-          style="max-height: 240px"
-        />
-        <h4>
+          style="max-height: 160px;"
+        /> </span> </a>
+        <h1 class="fs-4">
           Your AI Assistant for Career Guidance <br />
           & College Counseling
-        </h4>
+        </h1>
       </div>
 
       <div class="examples text-center mt-5">
-        <h5 class="text-secondary pt-5">Try asking about</h5>
+        <h2 class="text-secondary pt-5 fs-5">Try asking about</h2>
 
         <div class="d-flex flex-column align-items-center">
-          <button type="button" data-prompt="Top 10 IITs for Computer Science" class="btn btn-outline-secondary rounded-pill my-2">
+          <button type="button" data-prompt="Top 10 IITs for Computer Science"  aria-label="example 1" class="btn btn-outline-secondary rounded-pill my-2">
             "Top 10 IITs for Computer Science" →
           </button>
-          <button type="button" data-prompt="Preparation strategy for JEE Mains" class="btn btn-outline-secondary rounded-pill my-2">
+          <button type="button" data-prompt="Preparation strategy for JEE Mains" aria-label="example 2" class="btn btn-outline-secondary rounded-pill my-2">
             "Preparation strategy for JEE Mains" →
           </button>
-          <button type="button" data-prompt="Rank Prediction for JEE Mains" class="btn btn-outline-secondary rounded-pill my-2">
+          <button type="button" data-prompt="Rank Prediction for JEE Mains" aria-label="example 3" class="btn btn-outline-secondary rounded-pill my-2">
             "Rank Prediction for JEE Mains" →
           </button>
         </div>
@@ -41,11 +41,10 @@
 
       <div class="conversations mt-5 pb-5 px-4"></div>
 
-    <div class="footer position-fixed navbar-fixed  bottom-0 pt-1 bg-white">
+    <div class="footer position-fixed navbar-fixed shadow-sm bottom-0 bg-white">
 
-        <button type="button" class="btn btn-clear btn-outline-dark btn-sm rounded-pill my-3 mx-2" id="clear">
-          <i class="bi bi-eraser-fill fs-5"></i> Clear Conversations
-        </button> 
+        <button type="button" aria-label="Clear Conversations" class="btn btn-clear btn-outline-dark btn-sm rounded-pill my-2 mx-2 fs-6" id="clear">
+          <i class="bi bi-eraser-fill"></i> Clear </button> 
 
         <div class="text-center d-flex mb-3">
           
@@ -56,7 +55,7 @@
             placeholder="Type your message here"
           ></textarea>
 
-          <button id="send" class="btn btn-transparent send">
+          <button id="send" aria-label="Send Message" class="btn btn-transparent send">
             <i class="bi bi-send-fill"></i>
           </button>
 
